b6cf66b460b35b2bf2c38d603dc6bd6e52bccead
Vaishnavi18883 <patilvaishnavi1022@gmail.com>
2025-09-26 12:49:04 +0530

    swap.js

--- swap.js
+++ swap.js
@@ -1,24 +1,17 @@
 let a = 10;
 let b = 20;
-
-// let c = a;
-// a=b;
-// b=c;
-
-// console.log("vale is a",a);
-// console.log("vale is b",b);
-// c= a+b;  //30
-// a= c-a; //30-10 =20
-// b=c-a;
-// console.log(a);
-// console.log(b);
-
-
-
-
-
-
-[a,b]=[b,a]
+console.log("vale is a",a);
+console.log("vale is b",b);
+c= a+b;  //30
+a= c-a; //30-10 =20
+b=c-a;
 console.log(a);
 console.log(b);
+
+
+
+
+
+
+
 
