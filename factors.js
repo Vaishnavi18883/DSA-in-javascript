1527d96b69835d7ca53d99543f4e8e92742e54f3
Vaishnavi18883 <patilvaishnavi1022@gmail.com>
2025-09-27 10:37:59 +0530

    factors.js

--- factors.js
+++ factors.js
@@ -1,13 +1,9 @@
 var n = 4;
-
 for (let i = 1; i <= Math.floor(n/2); i++) {
     if(n%i===0){
-        console.log(i);
-   
-    
-        
+        console.log(i);       
     }
 }
-    
-     console.log(n);
-//
+console.log(n);
+
+
