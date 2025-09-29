7ac987f801197f8b6519010483a2a547dc38ed67
Vaishnavi18883 <patilvaishnavi1022@gmail.com>
2025-09-29 09:45:14 +0530

    positive.js

--- positive.js
+++ positive.js
@@ -1,11 +1,4 @@
-// let num=0;
-// console.log(num>0?"positive":num<0? "negative":"zero");
-// JavaScript uses the IEEE 754 standard for representing numbers.
+let num=0;
+console.log(num>0 ? "positive": num<0 ? "negative": "zero");
 
-// All numbers are 64-bit floating-point (called "double precision").
 
-// In this format, not all decimal fractions can be represented exactly in binary.
-
-let a=0.2;
-let b =0.1;
-console.log(a+b);//0.30000004
