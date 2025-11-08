c31da2a5017d07d726ee99d9ae38b213f3b032a5
Vaishnavi D Patil <patilvaishnavi1022@gmail.com>
2025-11-08 09:53:34 +0530

    areaPerimeter.js

--- areaPerimeter.js
+++ areaPerimeter.js
@@ -1,5 +1,8 @@
-let l = 20;
-let b= 12;
-console.log("Area of rectangle is",l*b);
-console.log("perimeter of rectangle",2(l*b));
+let prompt = require('prompt-sync')()
+let l = Number(prompt("Enter length of rectangle"));
+let b = Number(prompt("Enter breadth of rectangle"));
+let area = l*b;
+let perimeter = 2*(l+b);
+process.stdout.write("Area of rectangle is"+area +"\n");
+process.stdout.write("perimeter of rectangle"+perimeter +"\n");
 
