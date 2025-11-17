7cdd0b8eaec99810f8a939496a87969039c4b3e2
Vaishnavi D Patil <patilvaishnavi1022@gmail.com>
2025-11-17 08:21:22 +0530

    Add right-aligned pattern printing in JavaScript

---
+++ rightAllignedPattern.js
@@ -0,0 +1,12 @@
+let n = 5;
+for(let i=1; i<=n; i++){
+    let line = "";
+    for(let space=1; space<= n-i; space++){
+        line += " ";
+    }
+    for(let j=1; j<=i; j++){
+        line += "*"
+    }
+    console.log(line);
+    
+}
