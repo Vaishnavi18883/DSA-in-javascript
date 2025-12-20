b1fca379e06331e2fc9828d34903de8a14d97ce1
Vaishnavi D Patil <patilvaishnavi1022@gmail.com>
2025-12-20 21:52:27 +0530

    Add files via upload

---
+++ electricitybill.js
@@ -0,0 +1,14 @@
+let unit= 600
+let amt= 0;
+if(unit>400){
+    amt=(unit-400)*13
+    unit= 400;
+}if(unit>200 && unit<=400){
+    amt+=(unit-200)*8
+    unit=200
+}if(unit>100 && unit<=200){
+    amt+=(unit-100)*6
+  unit=100
+}
+amt+=unit*4;
+console.log(amt);
