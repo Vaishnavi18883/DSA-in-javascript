d7bd864fb6b25ca999cc26ac826b3edb468ff306
Vaishnavi D Patil <patilvaishnavi1022@gmail.com>
2026-02-23 21:28:53 +0530

    isAnagram function
    Implement function to check if two strings are anagrams.

---
+++ Anagram.js
@@ -0,0 +1,20 @@
+function isAnagram(str1, str2) {
+
+    if(str1.length !== str2.length){
+        return false
+    }
+     let count = {}
+    for(let i= 0;i< str1.length;i++){
+        let char = str1[i]
+        if(char[count]||0)+1
+           
+        
+    }
+    for(let i= 0;i<str2.length;i++){
+        let char = str2[i]
+        if(!count[char])
+            return false
+        count[char]--
+    }
+    return true
+}
