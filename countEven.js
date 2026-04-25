ff0b6fad57bd634ecc6367a8b2a4e843b7dd7642
Vaishnavi D Patil <patilvaishnavi1022@gmail.com>
2026-04-25 10:31:13 +0530

    function to count even numbers in an array

---
+++ countEven.js
@@ -0,0 +1,10 @@
+function countEven (arr) {
+let count = 0;
+for(let i = 0; i<arr.length; i++){
+    if(arr[i]%2 === 0){
+        count++
+    }
+}
+    return count;
+}
+console.log(countEven([2,4,3,1,22,44,6,7]));
