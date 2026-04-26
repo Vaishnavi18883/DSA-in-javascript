afaac4595797ba09d8a025d334138afd4b3d2106
Vaishnavi D Patil <patilvaishnavi1022@gmail.com>
2026-04-26 16:43:25 +0530

    Implement right rotation of array by k positions

---
+++ rightRotationByK.js
@@ -0,0 +1,11 @@
+let arr = [1,2,3,4,5];
+let k= 2;
+k= k%arr.length;
+for(let j= 0; j<k; j++){
+    let copy = arr[arr.length-1]
+    for(let i = arr.length-1; i>0;i--){
+        arr[i]= arr[i-1]
+    }
+    arr[0]= copy
+}
+console.log(arr);
